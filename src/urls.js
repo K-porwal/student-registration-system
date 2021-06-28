9d83765e7d2fb4fc3d950578a41c3a0aa06616b0
Kshitij Porwal <kshporwa@publicisgroupe.net>
2021-06-28 14:14:27 +0530

    Added corresponding prod url

--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,3 @@
 const DEV_URL = 'http://localhost:3001/';
-const PROD_URL = 'https://[APP-NAME].herokuapp.com/';
+const PROD_URL = 'https://st-registration-system-api.herokuapp.com/';
 export const API_ROOT = process.env.NODE_ENV === 'development' ? DEV_URL : PROD_URL;
